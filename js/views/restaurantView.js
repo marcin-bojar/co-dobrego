bf258c795cadae4dcc53336b1de4328af36dc51b
marcin-bojar <marcin.bojar@interia.pl>
2020-07-20 09:57:15 +0200

    Subtle radial-gradient in restaurant's details popup

--- js/views/restaurantView.js
+++ js/views/restaurantView.js
@@ -1,7 +1,7 @@
 import { elements } from '../base.js';
 
 export const renderRestaurantDetails = (x, y, restaurant) => {
-    elements.restaurantDetails.style.transformOrigin = `calc(${x}px - 62%) calc(${y}px - 58%)`;
+    elements.restaurantDetails.style.transformOrigin = `calc(${x}px - 73%) calc(${y}px - 58%)`;
     elements.restaurantDetails.classList.add('active');
     elements.overlay.classList.add('active');
     
@@ -21,7 +21,7 @@ export const renderRestaurantDetails = (x, y, restaurant) => {
                 <li class="restaurant-details__item">
                     <p><h3 class="heading-3">Adres:</h3> ${restaurant.location.address}</p>
                 </li>
-                ${restaurant.phone_numbers == 'Not available for this place' ?
+                ${restaurant.phone_numbers !== 'Not available for this place' ?
                 `<li class="restaurant-details__item"><h3 class="heading-3">Numer telefonu:</h3> ${restaurant.phone_numbers}</li>`
                 : ''}            
             </ul>
@@ -30,6 +30,7 @@ export const renderRestaurantDetails = (x, y, restaurant) => {
 };
 
 export const hideRestaurantDetails = () => {
-      elements.restaurantDetails.classList.remove('active');
-      elements.overlay.classList.remove('active');
+    elements.restaurantDetails.classList.add('hide');
+    setTimeout( () => elements.restaurantDetails.classList.remove('active', 'hide'), 500);
+    elements.overlay.classList.remove('active');
 };
