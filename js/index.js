1d5a02e034eb58f91fe8f0f168dba9d98cfb871d
marcin-bojar <marcin.bojar@interia.pl>
2020-08-15 21:22:36 +0200

    Added results animation when fetching data

--- js/index.js
+++ js/index.js
@@ -14,193 +14,192 @@ const state = {};
 // Search Controller //
 //////////////////////
 
-const searchCtrl = async (id=null, type=null, start=null) => {
+const searchCtrl = async (id = null, type = null, start = null) => {
+  //If parameters are provided then pagination button was clicked or city has been selected from list so...
+  if (id !== null && type !== null && start !== null) {
+    //Display loader...
+    searchView.renderLoader();
 
-    //If parameters are provided then pagination button was clicked or city has been selected from list so...
-    if(id !== null && type !== null && start !== null) {
+    // ...scroll to the top of results...
+    elements.title.scrollIntoView();
 
-        //Display loader...
-        searchView.renderLoader();
+    // ...animate old results...
+    elements.results.classList.add('fetching');
 
-        // ...scroll to the top of results...
-        elements.title.scrollIntoView();
+    // ...search for (another) set of restaurants ('start' is responsible for the first item in the list)...
+    await state.search.searchRestaurants(id, type, start);
 
-        // ...search for (another) set of restaurants ('start' is responsible for the first item in the list)...
-        await state.search.searchRestaurants(id, type, start);
+    //... animate new results...
+    elements.results.classList.remove('fetching');
 
-        //...hide loader...
-        searchView.hideLoader();
-        
+    //...hide loader...
+    searchView.hideLoader();
 
-        // ...display restaurants in UI...
-        searchView.renderRestaurantsList(state.search.searchDetails.restaurants);
+    // ...display restaurants in UI...
+    searchView.renderRestaurantsList(state.search.searchDetails.restaurants);
 
-        // ...and display pagination buttons if needed
-        searchView.renderPaginationButtons(state.search.searchDetails, id);
+    // ...and display pagination buttons if needed
+    searchView.renderPaginationButtons(state.search.searchDetails, id);
 
     // If no parameters were provided then search form was submitted
-    } else {
-        // Hide pagination buttons
-        searchView.hidePaginationButtons();
-        
-        //Clear previous results
-        searchView.clearResults();
+  } else {
+    // Hide pagination buttons
+    searchView.hidePaginationButtons();
 
-        //Display loader
-        searchView.renderLoader();
-        
-        //Get the location query from input
-        const query = searchView.getInput();
-    
-        // Check input
-        if(query) {
-            // Update the state object
-            state.search = new Search(query);
+    //Clear previous results
+    searchView.clearResults();
 
-            // Parse the query in search object
-            state.search.parseQuery();
+    //Display loader
+    searchView.renderLoader();
 
-            // Search for cities
-            try {
-                await state.search.searchForLocations();
-        
-                // If there are multipule matches for the city name entered by user show all possibilities
-                if(state.search.cityMatches && state.search.cityMatches.length > 1) {
-                    
-                    //Hide loader
-                    searchView.hideLoader();
+    //Get the location query from input
+    const query = searchView.getInput();
 
-                    // Display found cities
-                    searchView.renderCityResults(state.search.cityMatches);
-        
-                // If only one city matches the user's input then search for the restaurants in this city at once...
-                } else if (state.search.cityMatches.length === 1) {
-                    const cityID = state.search.cityMatches[0].id;
-                    await state.search.searchRestaurants(cityID);
+    // Check input
+    if (query) {
+      // Update the state object
+      state.search = new Search(query);
 
-                    // (hide loader meanwhile)
-                    searchView.hideLoader();
-                   
-                    // ...and display them in UI...
-                    searchView.renderRestaurantsList(state.search.searchDetails.restaurants);
-    
-                    // ...along with pagination buttons if needed
-                    searchView.renderPaginationButtons(state.search.searchDetails, cityID);
+      // Parse the query in search object
+      state.search.parseQuery();
 
-                    // Scroll to the top of results
-                    elements.title.scrollIntoView();
-    
-                    // console.log(state);
-    
-                // If there is no match for the city entered by user
-                } else {
-                    searchView.renderErrorMsg('Tego miasta nie ma w naszej bazie danych, przepraszamy...');
-                    searchView.hideLoader();
-                }   
-            }catch (err) {
-                alert('Houston, mamy problem!' + ' ' + err);
-                searchView.hideLoader();
-            }
-            
-        // If no input was provided
+      // Search for cities
+      try {
+        await state.search.searchForLocations();
+
+        // If there are multipule matches for the city name entered by user show all possibilities
+        if (state.search.cityMatches && state.search.cityMatches.length > 1) {
+          //Hide loader
+          searchView.hideLoader();
+
+          // Display found cities
+          searchView.renderCityResults(state.search.cityMatches);
+
+          // If only one city matches the user's input then search for the restaurants in this city at once...
+        } else if (state.search.cityMatches.length === 1) {
+          const cityID = state.search.cityMatches[0].id;
+          await state.search.searchRestaurants(cityID);
+
+          // (hide loader meanwhile)
+          searchView.hideLoader();
+
+          // ...and display them in UI...
+          searchView.renderRestaurantsList(
+            state.search.searchDetails.restaurants
+          );
+
+          // ...along with pagination buttons if needed
+          searchView.renderPaginationButtons(
+            state.search.searchDetails,
+            cityID
+          );
+
+          // Scroll to the top of results
+          elements.title.scrollIntoView();
+
+          // console.log(state);
+
+          // If there is no match for the city entered by user
         } else {
-            state.search = null;
-            alert('Podaj miejscowość, proszę...');
-            searchView.hideLoader();
+          searchView.renderErrorMsg(
+            'Tego miasta nie ma w naszej bazie danych, przepraszamy...'
+          );
+          searchView.hideLoader();
         }
+      } catch (err) {
+        alert('Houston, mamy problem!' + ' ' + err);
+        searchView.hideLoader();
+      }
 
+      // If no input was provided
+    } else {
+      state.search = null;
+      alert('Podaj miejscowość, proszę...');
+      searchView.hideLoader();
     }
+  }
 };
 
 // Search event listeners
 
 //Handle sumbit event on search form
 elements.form.addEventListener('submit', e => {
-    e.preventDefault();
-    searchCtrl();
+  e.preventDefault();
+  searchCtrl();
 });
 
 // Clear the input when focused out
-elements.where.addEventListener('focusout', e => e.target.value = '');
+elements.where.addEventListener('focusout', e => (e.target.value = ''));
 
 // Handle click event on pagination buttons
 elements.pages.addEventListener('click', e => {
-    const entity_id = e.target.dataset.entity_id;
-    const entity_type = e.target.dataset.entity_type;
-    const start = e.target.dataset.start;
+  const entity_id = e.target.dataset.entity_id;
+  const entity_type = e.target.dataset.entity_type;
+  const start = e.target.dataset.start;
 
-    if(e.target.tagName === 'BUTTON') {
-        searchCtrl(entity_id, entity_type, start);
-    }
-    
+  if (e.target.tagName === 'BUTTON') {
+    searchCtrl(entity_id, entity_type, start);
+  }
 });
 
 // Handle click event on item in city results list
 elements.results.addEventListener('click', e => {
-    
-    if(e.target.closest('div').className === 'results__city') {
-        const cityID = e.target.closest('div').dataset.id;
-        searchCtrl(cityID, 'city', 0);
-    }
+  if (e.target.closest('div').className === 'results__city') {
+    const cityID = e.target.closest('div').dataset.id;
+    searchCtrl(cityID, 'city', 0);
+  }
 
-    // Handle click event on item in restaurants list
-    if(e.target.closest('div').className === 'results__restaurant') {
-        restaurantCtrl(e);
-    }
+  // Handle click event on item in restaurants list
+  if (e.target.closest('div').className === 'results__restaurant') {
+    restaurantCtrl(e);
+  }
 });
-
-
 
 /////////////////////////////
 // Restaurant Controller //
 ///////////////////////////
 
-const restaurantCtrl = (e) => {
-    // Get the index of the restaurant in results' array in search object
-    const index = +e.target.closest('div').dataset.index;
-    
-    // Create new resutaurant object
-    const restaurant = new Restaurant(index);
+const restaurantCtrl = e => {
+  // Get the index of the restaurant in results' array in search object
+  const index = +e.target.closest('div').dataset.index;
 
-    // Get restaurant data from state object
-    restaurant.getRestaurantDetails(state);
+  // Create new resutaurant object
+  const restaurant = new Restaurant(index);
 
-    // Add it to state object
-    state.restaurant = restaurant;
+  // Get restaurant data from state object
+  restaurant.getRestaurantDetails(state);
 
-    // Retrieve the position of mouse in the moment when click happened (used as transform origin of restaurant details popup animation)
-    const {x, y} = restaurant.getMousePosition(e);
+  // Add it to state object
+  state.restaurant = restaurant;
 
-    // Display restaurants details in UI
-    restaurantView.renderRestaurantDetails(x, y, restaurant);
+  // Retrieve the position of mouse in the moment when click happened (used as transform origin of restaurant details popup animation)
+  const { x, y } = restaurant.getMousePosition(e);
 
+  // Display restaurants details in UI
+  restaurantView.renderRestaurantDetails(x, y, restaurant);
 };
 
 // Restaurant event listeners
 
 //Close the restaurant details popup with the 'x' icon
 elements.restaurantDetails.addEventListener('click', e => {
-    if(e.target.className === 'restaurant-details__close')
-        restaurantView.hideRestaurantDetails();
+  if (e.target.className === 'restaurant-details__close')
+    restaurantView.hideRestaurantDetails();
 });
 
 // Close the restaurant details popup by clicking anywhere outside it
 elements.overlay.addEventListener('click', () => {
-    restaurantView.hideRestaurantDetails();
+  restaurantView.hideRestaurantDetails();
 });
-
-
-
 
 /////////////////////////////
 // Geolocation Controller //
 ///////////////////////////
 
 const geoCtrl = () => {
-    // Get the location of user's device
-    getUserLocation();    
+  // Get the location of user's device
+  getUserLocation();
 };
-
 
 // Geolocation event listeners
 elements.locationBtn.addEventListener('click', geoCtrl);
